eb8857c29bcea648c1d2ec0336c6858282132b1b
Francisco Riadigos <riadigos.fran@gmail.com>
2018-09-04 16:37:58 +0100

    Improvements on Automate & Fixed issue when multiple actions fire at once on a BroadcastReceiver

--- component-interactive-notification/src/main/java/com/chattylabs/component/interactive/notification/InteractiveNotificationService.kt
+++ component-interactive-notification/src/main/java/com/chattylabs/component/interactive/notification/InteractiveNotificationService.kt
@@ -2,17 +2,25 @@ package com.chattylabs.component.interactive.notification
 
 import android.app.IntentService
 import android.content.Intent
+import com.chattylabs.component.interactive.notification.InteractiveNotificationComponentImpl.*
+import java.util.ArrayList
 
 class InteractiveNotificationService : IntentService("InteractiveNotificationService") {
+
+    private val consumedActions: ArrayList<String> = arrayListOf()
+
+    // FIXME: PASS STRUCTURE AS PARCEL!!!
+
     override fun onHandleIntent(intent: Intent?) {
         intent?.run {
             val actionId = InteractiveNotification.Utils.getActionId(this)
             val isDismissed = InteractiveNotification.Utils.isDismissed(this)
-            val component: InteractiveNotificationComponentImpl =
-                    InteractiveNotificationComponentImpl.Instance.get() as InteractiveNotificationComponentImpl
+            val component = Instance.get() as InteractiveNotificationComponentImpl
+            if (consumedActions.contains(actionId)) return
             if (!isDismissed) component.currentNode = component.getNode(actionId!!)
             applicationContext.sendBroadcast(
                     Intent(applicationContext, component.receiver).putExtras(extras!!))
+            consumedActions.add(actionId!!)
         }
     }
 }
